8e29d57c2d0c070913e05aae4c3268888938adac
Pedro Barros <pedrobars7a@gmail.com>
2021-05-14 13:25:18 -0300

    primeiro deploy

---
+++ src/config/server.js
@@ -0,0 +1,18 @@
+const port = process.env.PORT
+
+// const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
+
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(allowCors)
+server.use(queryParser())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
